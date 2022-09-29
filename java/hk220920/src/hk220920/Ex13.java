9a48728f31743ff70eee2c897a62e591ede5036e
Yeseulyo <113503449+Yeseulyo@users.noreply.github.com>
2022-09-29 15:18:29 +0900

    Add files via upload

---
+++ java/hk220920/src/hk220920/Ex13.java
@@ -0,0 +1,30 @@
+package hk220920;
+
+import java.util.Scanner; // 컨트롤 시프트 영문오 :  임폴트 단축키
+
+public class Ex13 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("시작 단을 입력해 주세요. : ");
+		int startDan = sc.nextInt();
+		System.out.print("마지막 단을 입력해 주세요. :");
+		int endDan = sc.nextInt();
+		System.out.print("시작 곱을 입력해 주세요. :");
+		int startGop = sc.nextInt();
+		System.out.print("마지막 곱을 입력해 주세요. :");
+		int endGop = sc.nextInt();
+		
+		int dan = startDan;
+		while(dan <= endDan) {
+			int gop = startGop;
+			while(gop <= endGop) {
+			System.out.println(dan + "*" + gop + "=" + dan * gop);
+			gop++;
+			}
+			dan++;
+		}
+		
+		
+		
+	}
+}
---
+++ java/hk220920/src/hk220920/Ex17.java
@@ -0,0 +1,45 @@
+package hk220920;
+
+public class Ex17 {
+	public static void main(String [] args) {
+		// continue
+		// 7단 출력
+		for(int gop = 1; gop <= 9; gop++) {
+			System.out.println("7 * "  + gop + " = " + 7 * gop );
+			
+		}
+		// 홀수곱만 출력하시오
+		for(int gop = 1; gop <= 9; gop++) {
+			System.out.println("7 * "  + gop + " = " + 7 * gop );
+		}
+		for(int gop = 1; gop <= 9; gop++) {
+			System.out.println("7 * "  + gop + " = " + 7 * gop );
+		}
+		for(int gop = 1; gop <= 9; gop++) {
+			if(gop % 2 == 0) continue;
+			System.out.println("7 * "  + gop + " = " + 7 * gop );
+		}
+		// 짝수곱만 출력하시오
+		for(int gop = 1; gop <= 9; gop++) {
+			if(gop % 2 == 1) continue;
+			System.out.println("7 * "  + gop + " = " + 7 * gop );
+		}
+		// while 짝수만 출력
+		int gop = 0;
+		while(gop <= 8) {
+			gop++;
+			if(gop % 2 == 1) continue;
+			System.out.println("7 * "  + gop + " = " + 7 * gop );
+		
+		}
+		// while 홀수만 출력
+		gop = 0;
+		while(gop <= 8) {
+			gop++;
+			if(gop % 2 == 0) continue;
+			System.out.println("7 * "  + gop + " = " + 7 * gop );
+			
+		}
+		
+	}
+}
