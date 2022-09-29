59e579bde83d09324e54e890e6e94675b7d4e8e4
Yeseulyo <113503449+Yeseulyo@users.noreply.github.com>
2022-09-29 15:16:17 +0900

    Add files via upload

---
+++ java/hk220915/src/hk220915/Variable3.java
@@ -0,0 +1,20 @@
+package hk220915;
+
+public class Variable3 {
+	public static void main(String [] args) {
+		// 변수 명명법
+		int level;
+		int _level;
+		//int 3level; 오류
+		int level2;
+		int testValiableTest; //변수의 첫번째 단어는 소문자를 써야한다(클래스라고 생각할 수 있어서 (가독성)
+		//int Valiable3;
+		/* 예약어는 변수명으로 사용할 수 없다.
+		int if; // 예약어(이미 지정되어 있는 것) ,자주색은 예약어
+		int for; 
+		int class
+		int public;
+		*/
+		
+	}
+}
